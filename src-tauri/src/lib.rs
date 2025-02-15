1f6ceed59df38fa984d507e38ef9078f4c045ed3
WASHUO <shuo.wang@mercedes-benz.com>
2025-02-15 23:25:10 +0800

    feat: add tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,15 @@
-use tauri::{AppHandle, Manager};
+use tauri::{
+    image::Image,
+    menu::{CheckMenuItem, MenuBuilder, MenuItem, SubmenuBuilder},
+    tray::TrayIconBuilder,
+    AppHandle, Manager,
+};
 
-#[tauri::command]
-fn open_main_window(app: AppHandle) {
+fn open_main_window(app: &AppHandle) {
     #[cfg(target_os = "macos")]
     {
-        tauri::AppHandle::show(&app).unwrap();
+        tauri::AppHandle::show(app).unwrap();
     }
-    // app.get_webview_window("main").unwrap().show().unwrap();
 }
 
 #[tauri::command]
@@ -18,12 +21,68 @@ fn close_app(app: AppHandle) {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![open_main_window, close_app])
+        .invoke_handler(tauri::generate_handler![close_app])
         .setup(|app| {
             #[cfg(target_os = "macos")]
             {
                 app.set_activation_policy(tauri::ActivationPolicy::Accessory);
             }
+            let icon_bytes = include_bytes!("../icons/tray-icon.png");
+            let setting = MenuItem::with_id(app, "setting", "Setting", true, None::<&str>)?;
+            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+            let auto_model =
+                CheckMenuItem::with_id(app, "auto_model", "Auto Model", true, false, None::<&str>)?;
+            let proxy_model = CheckMenuItem::with_id(
+                app,
+                "proxy_model",
+                "Proxy Model",
+                true,
+                false,
+                None::<&str>,
+            )?;
+            let direct_model = CheckMenuItem::with_id(
+                app,
+                "direct_model",
+                "Direct Model",
+                true,
+                false,
+                None::<&str>,
+            )?;
+            let node = MenuItem::with_id(app, "node", "Node", true, None::<&str>)?;
+            let server = SubmenuBuilder::new(app, "Servers").item(&node).build()?;
+
+            let menu = MenuBuilder::new(app)
+                .item(&auto_model)
+                .item(&proxy_model)
+                .item(&direct_model)
+                .separator()
+                .item(&server)
+                .separator()
+                .item(&setting)
+                .separator()
+                .item(&quit)
+                .build()?;
+            let _tray = TrayIconBuilder::new()
+                .tooltip("Secure Connect")
+                .icon_as_template(true)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "setting" => {
+                        println!("setting menu item was clicked");
+                        open_main_window(app);
+                    }
+                    "quit" => {
+                        println!("quit menu item was clicked");
+                        app.exit(0);
+                    }
+                    _ => {
+                        println!("menu item {:?} not handled", event.id);
+                    }
+                })
+                .menu(&menu)
+                .icon(Image::from_bytes(icon_bytes)?)
+                .build(app)?;
+
             Ok(())
         })
         .on_window_event(|win, event| {
--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
-import { Menu } from '@tauri-apps/api/menu';
-import { TrayIcon, TrayIconEvent } from '@tauri-apps/api/tray';
-import appIcon from './assets/app-icon.svg'
-
-const menu = await Menu.new({
-  items: [
-    {
-      id: 'settings',
-      text: 'Settings',
-      action: async () => {
-        console.log('settings pressed');
-        await invoke('open_main_window', [])
-      },
-    },
-    {
-      id: 'quit',
-      text: 'Quit',
-      action: async () => {
-        console.log('quit pressed')
-        await invoke('close_app', [])
-      },
-    },
-  ],
-});
-
-
-const options = {
-  menu,
-  action: (event: TrayIconEvent) => {
-    switch (event.type) {
-      case 'Click':
-        console.log(
-          `mouse ${event.button} button pressed, state: ${event.buttonState}`
-        );
-        break;
-      case 'DoubleClick':
-        console.log(`mouse ${event.button} button pressed`);
-        break;
-      case 'Enter':
-        console.log(
-          `mouse hovered tray at ${event.rect.position.x}, ${event.rect.position.y}`
-        );
-        break;
-      case 'Move':
-        console.log(
-          `mouse moved on tray at ${event.rect.position.x}, ${event.rect.position.y}`
-        );
-        break;
-      case 'Leave':
-        console.log(
-          `mouse left tray at ${event.rect.position.x}, ${event.rect.position.y}`
-        );
-        break;
-    }
-  },
-};
-
-const tray = await TrayIcon.new(options);
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  useEffect(() => {
+
+
+
+  }, [])
 
   return (
     <main className="w-full h-full flex justify-center p-2 bg-gray-100">
